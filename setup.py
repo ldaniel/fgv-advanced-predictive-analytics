199ecc6b8bbf5d63f9df9e9820ba0939b0eecd9a
ldaniel <contato@leandrodaniel.com>
2020-03-08 12:45:22 -0300

    Create initial structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Practicing Advanced Statistical Modelling in Python (Machine Learning I) ',
+    author='Leandro Daniel',
+    license='MIT',
+)
